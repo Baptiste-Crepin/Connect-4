082774d95e88890b801dcbe39f78614565637c9f
Baptiste-Crepin <81614462+Nypec@users.noreply.github.com>
2022-12-01 23:44:41 +0100

    clickable board + better arrow

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function getHeight() {
+  height = parseInt(document.getElementById("height").value);
+  if (height < 4) height = 4;
+  if (typeof height !== "number") height = 6;
+  return height;
+}
+
+function getWidth() {
+  width = parseInt(document.getElementById("width").value);
+  if (width < 4) width = 4;
+  if (typeof width !== "number") width = 7;
+  return width;
+}
+
+function other_player(player) {
+  if (player == 1) return 2;
+  return 1;
+}
+
 function createBoard(width, height) {
   //height*2 > width*2 ? (height = width*2-2, alert("vous avez rentré une hauteur trop grande par rapport a la largeur, celle-ci a été diminuée afin de pouvoir rentrer dans la page")): false;
 
@@ -12,17 +31,11 @@ function createBoard(width, height) {
   return board;
 }
 
-function other_player(player) {
-  if (player == 1) return 2;
-  return 1;
-}
-
 function insertpiece(board, x, player) {
   if (!(x > 0 && x <= width)) return false;
 
   for (let y = 0; y < board.length; y++) {
     if (board[y][x - 1] != 0) {
-      console.log(y);
       if (y == 0) return false;
       board[y - 1][x - 1] = player;
       return y - 1;
@@ -30,58 +43,6 @@ function insertpiece(board, x, player) {
       board[y][x - 1] = player;
       return y;
     }
-  }
-}
-
-function displayBoard(board) {
-  const htmlBoard = document.getElementById("board");
-  let numCol = "1fr ".repeat(width);
-  htmlBoard.style.gridTemplateColumns = numCol;
-  let insertion = "";
-
-  for (let boardRow = 0; boardRow < board.length; boardRow++) {
-    for (let boardCol = 0; boardCol < board[boardRow].length; boardCol++) {
-      //console.log(boardCol);
-      //console.log(boardCol, boardRow, board[boardCol][boardRow]);
-      switch (board[boardRow][boardCol]) {
-        case 0:
-          insertion += "<img src='./assets/blank.jpg'>";
-          break;
-        case 1:
-          insertion += "<img src='./assets/yellow.jpg'>";
-          break;
-        case 2:
-          insertion += "<img src='./assets/red.jpg'>";
-          break;
-      }
-    }
-  }
-  htmlBoard.innerHTML = insertion;
-
-  let child = Array.from(htmlBoard.children);
-  child.forEach((element) => {
-    element.style.width = (60 / width) * 0.5 + "vw";
-  });
-}
-
-function buttons(board) {
-  //creating the buttons
-  const htmlButtons = document.getElementById("buttons");
-  let numCol = "1fr ".repeat(width);
-  htmlButtons.style.gridTemplateColumns = numCol;
-  let insertion = "";
-  for (let boardCol = 0; boardCol < board[0].length; boardCol++) {
-    insertion +=
-      "<button style='white-space:pre-line; border-radius: 1rem;'>|\n|\nV</button>";
-  }
-  htmlButtons.innerHTML = insertion;
-
-  //buttons logic
-  let buttons = Array.from(htmlButtons.children);
-  for (let col = 0; col < buttons.length; col++) {
-    buttons[col].onclick = function () {
-      play(board, col);
-    };
   }
 }
 
@@ -156,31 +117,88 @@ function checkWin(board, row, col) {
   return false;
 }
 
-function stopPlay() {
-  const htmlButtons = document.getElementById("buttons");
-  let buttons = Array.from(htmlButtons.children);
-  for (let col = 0; col < buttons.length; col++) buttons[col].disabled = true;
+function displayBoard(board, clickable) {
+  const htmlBoard = document.getElementById("board");
+  let numCol = "1fr ".repeat(width);
+  htmlBoard.style.gridTemplateColumns = numCol;
+  let insertion = "";
+
+  for (let boardRow = 0; boardRow < board.length; boardRow++) {
+    for (let boardCol = 0; boardCol < board[boardRow].length; boardCol++) {
+      //console.log(boardCol);
+      //console.log(boardCol, boardRow, board[boardCol][boardRow]);
+      switch (board[boardRow][boardCol]) {
+        case 0:
+          insertion += "<img src='./assets/blank.jpg'>";
+          break;
+        case 1:
+          insertion += "<img src='./assets/yellow.jpg'>";
+          break;
+        case 2:
+          insertion += "<img src='./assets/red.jpg'>";
+          break;
+      }
+    }
+  }
+  htmlBoard.innerHTML = insertion;
+
+  if (clickable) clickBoard(board);
 }
 
-function getHeight() {
-  height = parseInt(document.getElementById("height").value);
-  if (typeof height !== "int") height = 6;
-  return height;
+function clickBoard(board) {
+  const htmlBoard = document.getElementById("board");
+  let cells = Array.from(htmlBoard.children);
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].onclick = function () {
+      play(board, i % 4);
+    };
+  }
 }
-function getWidth() {
-  width = parseInt(document.getElementById("width").value);
-  if (typeof width !== "int") width = 7;
-  return width;
+
+function buttons(board) {
+  //creating the buttons
+  const htmlButtons = document.getElementById("buttons");
+  let numCol = "1fr ".repeat(width);
+  htmlButtons.style.gridTemplateColumns = numCol;
+  let insertion = "";
+  for (let boardCol = 0; boardCol < board[0].length; boardCol++) {
+    insertion +=
+      "<img src='./assets/arrow_downward_FILL0_wght400_GRAD0_opsz48.svg'>";
+  }
+  htmlButtons.innerHTML = insertion;
+
+  //buttons logic
+  let buttons = Array.from(htmlButtons.children);
+  for (let col = 0; col < buttons.length; col++) {
+    buttons[col].onclick = function () {
+      play(board, col);
+      console.log("butt", col);
+    };
+  }
+}
+
+function stopPlay() {
+  displayBoard(board, false);
+
+  const BUTTONS = Array.from(document.getElementById("buttons").children);
+  for (let button of BUTTONS) {
+    button.onclick = "";
+    button.style.filter =
+      "invert(67%) sepia(0%) saturate(477%) hue-rotate(180deg) brightness(98%) contrast(87%)";
+  }
+
+  const CELLS = Array.from(document.getElementById("board").children);
+  for (let cell of CELLS) cell.onclick = "";
 }
 
 function initialize() {
+  gameOver = false;
   player = 1;
   width = getWidth();
   height = getHeight();
   board = createBoard(width, height);
+  displayBoard(board, true);
   buttons(board);
-  gameOver = false;
-  displayBoard(board);
 }
 
 function play(board, col) {
@@ -189,8 +207,10 @@ function play(board, col) {
 
   if (checkWin(board, row, col)) gameOver = true;
   if (gameOver) stopPlay();
-  displayBoard(board);
+  else displayBoard(board, true);
   player = other_player(player);
 }
 
 initialize();
+
+//put svg in red  | filter: invert(31%) sepia(82%) saturate(7295%) hue-rotate(354deg) brightness(106%) contrast(128%)
